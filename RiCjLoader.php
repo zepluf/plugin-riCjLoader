ca6f6d0c72dbb48f6f9612ffeb8f3e3f38973ec6
Raine Nguyen <vunguyen@rubikin.com>
2013-02-06 16:18:12 +0700

    minor changes to remove the use of ricache

--- RiCjLoader.php
+++ RiCjLoader.php
@@ -7,14 +7,17 @@ use Zepluf\Bundle\StoreBundle\Events;
 /**
  * the plugin main class
  */
-class RiCjLoader extends PluginCore{
-	public function init(){
+class RiCjLoader extends PluginCore
+{
+    public function init()
+    {
         global $autoLoadConfig;
         // we want to include the loader into the view for easy access, we need to do it after the template is loaded
         $autoLoadConfig[200][] = array('autoType' => 'require', 'loadFile' => __DIR__ . '/init_includes.php');
-	}
+    }
 
-    public function activate(){
+    public function activate()
+    {
         riMkDir($this->container->getParameter('web.cache_dir') . '/ricjloader/');
         return true;
     }
